c510022a3b226c72af90b70e5423aa06e4072f69
lucjan <lucjanjaroszewski@gmail.com>
2024-10-18 07:38:51 +0200

    add async

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const jokeBtn = document.querySelector('#jokeBtn');
 
 jokeBtn.addEventListener('click', generateJoke);
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   };
-  fetch('https://icanhazdadjoke.com/', config)
-    .then((res) => res.json())
-    .then((data) => {
-      joke.innerHTML = data.joke;
-    });
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+
+  const data = await res.json();
+  joke.innerHTML = data.joke;
 }
